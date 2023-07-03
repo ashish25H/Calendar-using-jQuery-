de51c4cd0d16e710291622be2c1e09fdeac9f6f4
ashish25H <me.ashishkp@gmail.com>
2023-07-03 14:37:58 +0530

    remove extra code

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ const monthDropdown = $('#month-dropdown');
 const yearDropdown = $('#year-dropdown');
 const monthText = $('#month');
 const yearText = $('#year');
-const monthRow1 = $('#month-row-1');
-const monthRow2 = $('#month-row-2');
-const monthRow3 = $('#month-row-3');
-const monthRow4 = $('#month-row-4');
-const monthRow5 = $('#month-row-5');
-const monthRow6 = $('#month-row-6');
+const ROWS = 6;
+const MIN_YEAR = 2010;
+const MAX_YEAR = 2040;
 const weekDaysArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 let startDate = '';
 let endDate = '';
-let incrementDecrementFlag = 0; //when you click on right arrow button it will   increase and it will decrease when you click on left arrow button.
+let flag = 0; //when you click on right arrow button it will   increase and it will decrease when you click on left arrow button.
 let selectedMonth = new Date().getMonth();
 let selectedYear = new Date().getFullYear();
-let checkFunctionCall = false;
+let removeExectingSelection = false;
+
 
 const addWeekDays = () => {         //adding week days like sun,mon,tue,wed etc.
     let str = '';
@@ -37,7 +35,7 @@ const setMonthsInDropdown = () => {         //set month name in month drop down
 }
 setMonthsInDropdown();
 
-let setYearsInDropdown = () => {        //set year in year drop down
+let setYearsInDropdown = () => {        //set years in year drop down
     let str = '';
     for (let i = 2010; i <= 2040; i++) {
         str += `<option value=${i}>${i}</option>`;
@@ -46,33 +44,16 @@ let setYearsInDropdown = () => {        //set year in year drop down
 }
 setYearsInDropdown();
 
-function addSunday(dateText, month, year) {
-    if (dateText === new Date().getDate() && month === new Date().getMonth() && year === new Date().getFullYear()) {
-        return `<div class='current-date month-date sunday grid-item' id=${dateText}>${dateText}</div>`;
-    } else {
-        return `<div class='month-date sunday grid-item' id=${dateText}>${dateText}</div>`;
-    }
-}
-
-function addInactiveDate(dateText) {
-    return `<div class='inactive-date grid-item'>${dateText}</div>`;
-}
-
-function addDate(dateText, month, year) {
-    if (dateText === new Date().getDate() && month === new Date().getMonth() && year === new Date().getFullYear()) {
-        return `<div class='month-date grid-item current-date' id=${dateText}>${dateText}</div>`;
-    } else {
-        return `<div class='date-item month-date grid-item' id=${dateText}>${dateText}</div>`;
-    }
-}
-
 function addDateRangeClass(startDate, endDate) {
-    checkFunctionCall = true;
+    console.log(`startDate - ${startDate}`);
+    console.log(`EndDatre - ${endDate}`);
+    console.log(`in addDaterange function`);
+    removeExectingSelection = true;
     let min = +startDate < +endDate ? +startDate : +endDate;
     let max = +startDate > +endDate ? +startDate : +endDate;
     for (let i = min; i <= max; i++) {
         const item = $(`#${i}`);
-        item.removeClass('date-item current-date');
+        item.removeClass('current-date');
         item.addClass('date-range');
     }
 }
@@ -106,21 +87,17 @@ function addDateRangeFeature() {
 
             if (prevStart === 0 || prevEnd === '' || prevStart !== startDate) {
                 if (prevStart !== startDate) {
-                    // console.log(`prevv and start is not equal`);
                     removeDateRangeClass(prevStart, prevEnd);
-                    // console.log(`it called`);
                 }
                 prevStart = startDate;
                 prevEnd = endDate;
             }
 
-            // console.log(`prevstr - ${prevStart} prevend - ${prevEnd} Start - ${startDate} endDate - ${endDate}`);
-
-            if (checkFunctionCall) {
+            if (removeExectingSelection) {
                 startDate = '';
                 endDate = '';
 
-                checkFunctionCall = false;
+                removeExectingSelection = false;
             }
 
             event.stopPropagation();
@@ -130,8 +107,11 @@ function addDateRangeFeature() {
 
 }
 
-function createCalender(selectedMonth = null, selectedYear = null) {
+function createCalendar(selectedMonth = null, selectedYear = null) {
     let dt = new Date();
+    let currentDate = new Date();
+    const startString = `<div class='grid-item `;
+    const endString = `</div>`;
 
     if (selectedMonth !== null && selectedYear !== null) {
         if (isNaN(selectedMonth)) {
@@ -144,9 +124,9 @@ function createCalender(selectedMonth = null, selectedYear = null) {
     monthDropdown.val(monthArray[dt.getMonth()]);
     yearDropdown.val(dt.getFullYear());
 
-    // console.log(incrementDecrementFlag);
-    if (incrementDecrementFlag !== 0) {
-        dt.setMonth(new Date().getMonth() + incrementDecrementFlag);
+    // console.log(flag);
+    if (flag !== 0) {
+        dt.setMonth(new Date().getMonth() + flag);
     }
 
     const month = dt.getMonth();
@@ -160,24 +140,33 @@ function createCalender(selectedMonth = null, selectedYear = null) {
     const dateObj = new Date(year, month, 1);
     const firstDayOfMonth = dateObj.getDay();
 
-    let x = 1;
-    let sunday = 1;
-    for (let i = 1; i <= 6; i++) {
+    // let count = 1;
+    let sunday = count = 1;
+    let lastDayOfPreviousMonth = new Date(year, month, 0).getDate('', month, year);
+    for (let i = 1; i <= ROWS; i++) {
         let rowElement = $(`#month-row-${i}`);
         let rowContent = '';
-        // console.log(rowContent);
-        for (let j = (i*7 - 6); j <= i * 7; j++) {
+    
+        for (let j = (i * 7 - ROWS); j <= i * 7; j++) {
             if (j <= firstDayOfMonth) {
-                let lastDayOfPreviousMonth = new Date(year, month, 0).getDate('', month, year);
-               rowContent += addInactiveDate((lastDayOfPreviousMonth -        firstDayOfMonth) + j);
+                //add inactive dates
+                rowContent += `${startString} inactive-date'> ${(lastDayOfPreviousMonth - firstDayOfMonth) + j} ${endString}`;
+
             } else if (j > numberOfDaysInMonth + firstDayOfMonth) {
-                rowContent += addInactiveDate(x);
-                x++;
+                //add inactive dates
+                rowContent += `${startString} inactive-date'> ${count} ${endString}`;
+                count++;
             } else if (j - sunday === 7 || j === 1) {
-                rowContent += addSunday(j - firstDayOfMonth, month, year);
+                //adding sunday here
+                let dateText = j - firstDayOfMonth;
+                rowContent += (dateText === currentDate.getDate() && month === currentDate.getMonth() && year === currentDate.getFullYear()) ? `${startString} current-date month-date sunday' id=${dateText}>${dateText}${endString}` : `${startString} month-date sunday' id=${dateText}> ${dateText} ${endString}`;
+
                 sunday = j;
+
             } else {
-                rowContent += addDate(j - firstDayOfMonth, month, year);
+                //add normal dates in calendar 
+                let dateText = j - firstDayOfMonth;
+                rowContent += (dateText === currentDate.getDate() && month === currentDate.getMonth() && year === currentDate.getFullYear()) ? `${startString} month-date current-date' id=${dateText}> ${dateText} ${endString}` : `${startString} month-date' id=${dateText}> ${dateText} ${endString}`;
             }
         }
 
@@ -187,49 +176,47 @@ function createCalender(selectedMonth = null, selectedYear = null) {
     addDateRangeFeature();
 }
 
-function initButtons() {
+function handleEvents() {
     $('#next-button').click(function (event) {
-        event.stopPropagation();
-        incrementDecrementFlag++;
-        createCalender();
+        flag++;
+        createCalendar();
     });
 
     $('#back-button').click(function (event) {
-        event.stopPropagation();
-        incrementDecrementFlag--;
-        createCalender();
+        flag--;
+        createCalendar();
     });
+
+    $('#current-date-btn').click(function (event) {
+        flag = 0;
+        selectedMonth = new Date().getMonth();
+        selectedYear = new Date().getFullYear();
+        createCalendar();
+    });
+
+    $('#month-dropdown').change(function (event) {
+        // event.stopPropagation();
+        selectedMonth = monthDropdown.val();
+        flag = 0;
+        createCalendar(selectedMonth, selectedYear);
+    });
+
+    $('#year-dropdown').change(function (event) {
+        // event.stopPropagation();
+        selectedYear = yearDropdown.val();
+        flag = 0;
+        createCalendar(selectedMonth, selectedYear);
+    });
+
+    // $('body').click(function(event, startDate, endDate){
+    //     if(event.target.classList.contains('body-class')){
+    //         console.log(`body event called`);
+    //         console.log(`${startDate} ${endDate} in body event`);
+    //         removeDateRangeClass(startDate, endDate);
+    //     }
+    // });
+    
 }
 
-$('#current-date-btn').click(function (event) {
-    event.stopPropagation();
-    incrementDecrementFlag = 0;
-    selectedMonth = new Date().getMonth();
-    selectedYear = new Date().getFullYear();
-    createCalender();
-});
-
-function selectingMonthFunction(selectedMonth) {
-    createCalender(selectedMonth, selectedYear);
-}
-
-function selectingYearFunction(selectedYear) {
-    createCalender(selectedMonth, selectedYear);
-}
-
-$('#month-dropdown').change(function (event) {
-    event.stopPropagation();
-    selectedMonth = monthDropdown.val();
-    incrementDecrementFlag = 0;
-    selectingMonthFunction(selectedMonth);
-});
-
-$('#year-dropdown').change(function (event) {
-    event.stopPropagation();
-    selectedYear = yearDropdown.val();
-    selectingYearFunction(selectedYear);
-});
-
-
-createCalender();
-initButtons();
+createCalendar();
+handleEvents();
